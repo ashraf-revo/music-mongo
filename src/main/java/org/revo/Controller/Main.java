6f4f5279d09d20a43fa366b75008b5b7b2c99cbf
ashraf <ashraf1abdelrasool@gmail.com>
2017-02-16 10:43:42 +0200

    solve field injection

--- src/main/java/org/revo/Controller/Main.java
+++ src/main/java/org/revo/Controller/Main.java
@@ -1,7 +1,5 @@
 package org.revo.Controller;
 
-import org.revo.Service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,14 +10,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Main {
-
-    private final UserService userService;
-
-    @Autowired
-    public Main(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping(value = {"", "/{[path:[^.]*}", "/{[path:[^.]*}/{[path:[^.]*}", "/{[path:[^.]*}/{[path:[^.]*}/{[path:[^.]*}", "/{[path:[^.]*}/{[path:[^.]*}/{[path:[^.]*}/{[path:[^.]*}"})
     public ModelAndView index() {
         return new ModelAndView("forward:/index.html");
